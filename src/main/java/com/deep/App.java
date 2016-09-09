a06835f75f0ee6591fd22fa0fd63d5c9aa87957f
Soumyadeep Ghosh Dastidar <soumyadeepghoshdastidar@Soumyadeeps-MacBook-Pro.local>
2016-09-10 00:22:37 +0530

    Work started in basic branch 22

--- src/main/java/com/deep/App.java
+++ src/main/java/com/deep/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! Deep is working from basic branch" );
+        System.out.print('Y');
     }
 }

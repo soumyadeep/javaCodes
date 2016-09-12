cf4df0332a712b35701c3451854bd6f42b612c99
Soumyadeep Ghosh Dastidar <soumyadeepghoshdastidar@Soumyadeeps-MacBook-Pro.local>
2016-09-12 18:26:54 +0530

    commit 1

--- src/main/java/com/deep/App.java
+++ src/main/java/com/deep/App.java
@@ -9,9 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Deep is working from basic branch" );
-<<<<<<< HEAD
         System.out.print('Y');
-=======
->>>>>>> 392d84f358c4570d1a27dbf8bed24d72e0e1fe7c
+        //commit - 1
     }
 }
